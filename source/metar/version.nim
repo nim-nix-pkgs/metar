5b87b70f152fedbed7318af54b71693b15bcec81
hugosenari <hugosenari@gmail.com>
2022-09-03 19:49:39 -0300

    feat(nimpkgs): update lock

--- source/metar/version.nim
+++
@@ -1,7 +0,0 @@
-## The version.nim file defines the version numbers of metar and its
-## required components.
-
-## Nimpy is not required for metar exe, just for the metar python lib.
-
-const metarVersion* = "0.1.25"
-const nimpyVersion* = "0.1.0-c8ec14a"
--- source/testfiles/readme.md
+++
@@ -1,3 +0,0 @@
-Try to only have one test file per file format and use small
-image files.  This keeps the size of the metar repository small
-which makes working with the project easier.
